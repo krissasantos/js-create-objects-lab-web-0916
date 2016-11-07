a7753fb942ad7bd64f38b815b8948a5b6750f643
krissasantos <krissa.santos31@gmail.com>
2016-11-07 10:02:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 'use strict';
+
+function Dog(name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+var dog = new Dog("Perry", "Schnauzer" , "91")
+
+function Cat(name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+var cat = new Cat ("Lola", "Persian" , "9")
+
+
+function Ferret(name, age, foodRequirements) {
+  this.name = name;
+  this.age = age;
+  this.foodRequirements = foodRequirements;
+}
+
+// var Ferret = new Ferret ("Bubu", "1", ["bacon", "pastrami"])
+
+function Bird(name, breed) {
+  this.name = name;
+  this.breed = breed;
+}
+
+function Fish(name, species, waterConditions) {
+  this.name = name;
+  this.species = species;
+  this.waterConditions = waterConditions;
+}
